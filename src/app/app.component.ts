52fa8b6c658a5786fe301527eaa06a848a5817de
Flavio Silva <fal_silva@hotmail.com>
2023-11-11 23:58:19 -0300

    Inicio do Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'primeiro-projeto';
+  title = 'Mundo do Angular';
 }
